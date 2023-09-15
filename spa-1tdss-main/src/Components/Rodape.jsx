d14460533617cb626cc0f8b007a93cfdebff4030
Victor Fusco Martins <105394403+Victor-FM@users.noreply.github.com>
2023-09-14 22:24:20 -0300

    Delete

--- spa-1tdss-main/src/Components/Rodape.jsx
+++
@@ -1,9 +0,0 @@
-export default function Rodape(){
-     return (
-        <>
-            <footer>
-                <p>&copy; Todos os meus direitos reservados. 2023</p>
-            </footer>
-        </>
-     )
-}
